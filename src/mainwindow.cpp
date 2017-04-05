ce46005ac0d82b17bc9e69b9294cff8c00f59515
Ivan Ristović <ivo.ristovic@gmail.com>
2017-04-05 22:43:04 +0200

    Create test scene with a rectangle

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,27 @@
 #include "include/mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Creating a test scene
+    QGraphicsScene *scene = new QGraphicsScene();
+
+    // Creating a rectangle
+    QGraphicsRectItem *rect = new QGraphicsRectItem();
+    rect->setRect(0,0,50,50);
+
+    // Add the rectangle into the scene
+    scene->addItem(rect);
+
+    // Add scene to main display widget
+    ui->gwDisplay->setScene(scene);
 }
 
 MainWindow::~MainWindow()
