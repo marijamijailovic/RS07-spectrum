a4104743746761b1dd250b9b7f609ecd413db50b
Ivan Ristović <ivo.ristovic@gmail.com>
2017-05-08 22:34:23 +0200

    Add color changer

---
+++ include/colorchanger.h
@@ -0,0 +1,35 @@
+#ifndef COLORCHANGER_H
+#define COLORCHANGER_H
+
+#include <QObject>
+#include <QTimer>
+#include <QGraphicsView>
+#include <QGraphicsEllipseItem>
+#include "include/colors.h"
+
+
+class ColorChanger : public QObject, public QGraphicsItem
+{
+    Q_OBJECT
+
+public:
+    ColorChanger(QGraphicsView *view, qreal x, qreal y, QColor color);
+    QRectF boundingRect() const override;
+    QPainterPath shape() const override;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+
+private slots:
+    void expand();
+
+private:
+    void run();
+
+    qreal _x;
+    qreal _y;
+    qreal _r;
+    QGraphicsView *_view;
+    QColor _color;
+    QScopedPointer<QTimer> _expandTicker;
+};
+
+#endif // COLORCHANGER_H
---
+++ src/colorchanger.cpp
@@ -0,0 +1,48 @@
+#include "include/colorchanger.h"
+
+ColorChanger::ColorChanger(QGraphicsView *view, qreal x, qreal y, QColor color) :
+    _x(x),
+    _y(y),
+    _r(50),
+    _view(view),
+    _color(color),
+    _expandTicker(new QTimer())
+{
+    setZValue(-1);
+    connect(_expandTicker.data(), SIGNAL(timeout()), this, SLOT(expand()));
+    _expandTicker->start(20);
+}
+
+QRectF ColorChanger::boundingRect() const
+{
+    return QRectF(_x, _y, _r, _r);
+}
+
+QPainterPath ColorChanger::shape() const
+{
+    QPainterPath path;
+    path.addRect(_x, _y, _r, _r);
+    return path;
+}
+
+void ColorChanger::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *)
+{
+    painter->setBrush(QBrush(_color));
+    painter->drawEllipse(_x - _r/2, _y - _r/2, _r, _r);
+}
+
+void ColorChanger::expand()
+{
+    if (_r <= 2000) {
+        _r += 50;
+        update(_x, _y, _r, _r);
+        _view->update();
+    } else
+        _expandTicker->stop();
+}
+
+void ColorChanger::run()
+{
+    _expandTicker->start(20);
+}
+
